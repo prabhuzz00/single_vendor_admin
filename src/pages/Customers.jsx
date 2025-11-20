bfc3963f38763e372cd8417eecf440534b5efabf
66biswajeet <biswajeetjena2003@gmail.com>
2025-11-20 08:52:54 +0530

    Ui_updates

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -97,7 +97,10 @@ const Customers = () => {
               </div>
               <div className="flex items-center gap-2 flex-grow-0 md:flex-grow lg:flex-grow xl:flex-grow">
                 <div className="w-full mx-1">
-                  <Button type="submit" className="h-12 w-full bg-brown-600 hover:bg-brown-700 rounded-lg shadow-sm hover:shadow-md">
+                  <Button
+                    type="submit"
+                    className="h-12 w-full bg-brown-600 hover:bg-brown-700 rounded-lg shadow-sm hover:shadow-md"
+                  >
                     Filter
                   </Button>
                 </div>
